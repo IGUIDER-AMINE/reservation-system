4b65d21fc5576b9f207eb7a0dd2d97e91502ca91
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-05-27 11:12:21 +0200

    back-end

---
+++ reservation-back-end/src/main/java/sdia/reservationbackend/ReservationBackEndApplication.java
@@ -0,0 +1,13 @@
+package sdia.reservationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationBackEndApplication.class, args);
+    }
+
+}
